730f6477c7a03fa13a6c60f88cc805838489cc59
JaeChen <470494530@qq.com>
2018-07-28 14:46:46 +0800

    pca_model.py

---
+++ pca_model.py
@@ -0,0 +1,20 @@
+import numpy as np 
+
+
+def normalize(X, axis=-1, p=2):
+    pass
+
+def standardize(X):
+    pass
+
+def train_test_split(X, Y, test_size=0.2, shuffle=True, seed=None):
+    pass
+
+def calculate_variance(X):
+    pass
+
+def calculate_std_dev(X):
+    pass
+
+def calculate_correlation_matrix(X, Y=np.empty([0])):
+    pass
